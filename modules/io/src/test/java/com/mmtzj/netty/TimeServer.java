dfc11527f857ae365afdeb69d8df5fda2446780b
fanlu <fanlu7311@163.com>
2013-05-11 22:59:16 +0800

    fix

--- modules/io/src/test/java/com/mmtzj/netty/TimeServer.java
+++ modules/io/src/test/java/com/mmtzj/netty/TimeServer.java
@@ -1,32 +1,49 @@
 package com.mmtzj.netty;
 
 import org.jboss.netty.bootstrap.ServerBootstrap;
-import org.jboss.netty.channel.ChannelFactory;
-import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.*;
+import org.jboss.netty.channel.group.ChannelGroup;
+import org.jboss.netty.channel.group.ChannelGroupFuture;
+import org.jboss.netty.channel.group.DefaultChannelGroup;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 
 public class TimeServer {
-  
-    public static void main(String[] args) throws Exception {   
+
+    static final ChannelGroup allChannels = new DefaultChannelGroup("time-server");
+
+    public static void main(String[] args) throws Exception {
         ChannelFactory factory =
-            new NioServerSocketChannelFactory(
-                    Executors.newCachedThreadPool(),   
-                    Executors.newCachedThreadPool());   
-  
-        ServerBootstrap bootstrap = new ServerBootstrap (factory);
-  
-        TimeServerHandler handler = new TimeServerHandler();
-        TimeEncoder encoder = new TimeEncoder();
-        ChannelPipeline pipeline = bootstrap.getPipeline();
-//        pipeline.addLast("handler", handler);
-        pipeline.addLast("handler", encoder);
-  
-        bootstrap.setOption("child.tcpNoDelay", true);   
-        bootstrap.setOption("child.keepAlive", true);   
-  
-        bootstrap.bind(new InetSocketAddress(8000));
-    }   
+                new NioServerSocketChannelFactory(
+                        Executors.newCachedThreadPool(),
+                        Executors.newCachedThreadPool());
+
+        ServerBootstrap bootstrap = new ServerBootstrap(factory);
+
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            @Override
+            public ChannelPipeline getPipeline() throws Exception {
+                return Channels.pipeline(new TimeEncoder(),
+                        new TimeServerHandler());
+            }
+        });
+
+        bootstrap.setOption("child.tcpNoDelay", true);
+        bootstrap.setOption("child.keepAlive", true);
+
+        Channel channel = bootstrap.bind(new InetSocketAddress(8000));
+
+        allChannels.add(channel);
+//        waitForShutdownCommand();
+//        ChannelGroupFuture future = allChannels.close();
+//        future.awaitUninterruptibly();
+//        factory.releaseExternalResources();
+    }
+
+    private static void waitForShutdownCommand() throws IOException {
+//        System.in.read();
+    }
 }
