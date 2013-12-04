61be062688c69e263d46f17d730393b2c2246117
lu.fan <lu.fan@langtaojin.com>
2013-12-04 22:30:51 +0800

    add base jdk1.6

---
+++ modules/base/src/test/java/BaseTest.java
@@ -0,0 +1,128 @@
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+import org.junit.Test;
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by fanlu on 13-12-4.
+ */
+public class BaseTest {
+
+
+    @Test
+    public void heapOOM() {
+
+        List<OOMObject> list = new ArrayList<OOMObject>();
+
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+
+    @Test
+    public void stackLeakTest() {
+        stackLeak();
+    }
+
+    private int stackLength = 1;
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public void dontStop() {
+        while (true) {
+
+        }
+    }
+
+    public void stackLeakByThread() {
+        while (true) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dontStop();
+                }
+            });
+            thread.start();
+        }
+    }
+
+    @Test
+    public void stackLeakByThreadTest() {
+        stackLeakByThread();
+    }
+
+    /**
+     * -XX:PermSize=10M -XX:MaxPermSize=10M
+     */
+    @Test
+    public void runtimeConstantPoolOOMTest() {
+        List<String> list = new ArrayList<String>();
+        int i = 0;
+        while (true) {
+            System.out.println(String.valueOf(i++).intern());
+            list.add(String.valueOf(i++).intern());
+        }
+    }
+
+    /**
+     * -XX:PermSize=10M -XX:MaxPermSize=10M
+     */
+    @Test
+    public void methodAreaOOMTest() {
+
+        while (true) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                @Override
+                public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                    return methodProxy.invokeSuper(o, objects);  //To change body of implemented methods use File | Settings | File Templates.
+                }
+            });
+            enhancer.create();
+        }
+    }
+
+    /**
+     * -Xmx20M -XX:MaxDirectMemorySize=10M
+     * @throws IllegalAccessException
+     */
+    @Test
+    public void directMemoryOOMTest() throws IllegalAccessException {
+        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+        unsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+        while (true){
+            unsafe.allocateMemory(1024 * 1024);
+        }
+    }
+
+    /**
+     * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:SurvivorRatio=8
+     */
+    @Test
+    public void allocationTest(){
+        int _1MB = 1024 * 1024;
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        allocation4 = new byte[4 * _1MB];
+    }
+
+    @Test
+    public void getArch(){
+        String arch = System.getProperty("sun.arch.data.model");
+        System.out.println(arch+"-bit");
+    }
+}
