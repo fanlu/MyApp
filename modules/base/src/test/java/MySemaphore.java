cb2a50eb9eddff0707727ca60c89ed29974ea930
lu.fan <lu.fan@langtaojin.com>
2014-01-15 15:40:58 +0800

    java.util.concurrent

--- modules/base/src/test/java/MySemaphore.java
+++ modules/base/src/test/java/MySemaphore.java
@@ -14,14 +14,14 @@ public class MySemaphore extends Thread {
     public void run() {
         try {
             if (position.availablePermits() > 0) {
-                System.out.println("顾客[" + this.id + "]进入厕所，有空位");
+                System.out.println(System.nanoTime() + "顾客[" + this.id + "]进入厕所，有空位");
             } else {
-                System.out.println("顾客[" + this.id + "]进入厕所，没空位，排队");
+                System.out.println(System.nanoTime() + "顾客[" + this.id + "]进入厕所，没空位，排队");
             }
             position.acquire();
-            System.out.println("顾客[" + this.id + "]获得坑位");
+            System.out.println(System.nanoTime() + "顾客[" + this.id + "]获得坑位");
             Thread.sleep((int) (Math.random() * 1000));
-            System.out.println("顾客[" + this.id + "]使用完毕");
+            System.out.println(System.nanoTime() + "顾客[" + this.id + "]使用完毕");
             position.release();
         } catch (Exception e) {
             e.printStackTrace();
@@ -36,7 +36,7 @@ public class MySemaphore extends Thread {
         }
         list.shutdown();
         position.acquireUninterruptibly(2);
-        System.out.println("使用完毕，需要清扫了");
+        System.out.println(System.nanoTime() + "使用完毕，需要清扫了");
         position.release(2);
     }
 }
