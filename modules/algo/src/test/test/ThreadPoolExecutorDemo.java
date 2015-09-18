366f7e0dc03bcfa006a8be3affc1a6f920037abb
fanlu <fanlu7311@163.com>
2015-09-18 08:42:57 +0800

    thread pool

--- modules/algo/src/test/test/ThreadPoolExecutorDemo.java
+++ modules/algo/src/test/test/ThreadPoolExecutorDemo.java
@@ -9,30 +9,43 @@ import java.util.concurrent.atomic.AtomicInteger;
  * To change this template use File | Settings | File Templates.
  */
 public class ThreadPoolExecutorDemo {
-//    final BlockingQueue<Runnable> queue = new SynchronousQueue<Runnable>();
+    //    final BlockingQueue<Runnable> queue = new SynchronousQueue<Runnable>();
     final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(15);
+    //最大支持 最大线程数+queue的大小
+    //int corePoolSize,
+//    int maximumPoolSize,
+//    long keepAliveTime,
+//    TimeUnit unit,
+//    BlockingQueue<Runnable> workQueue,
+//    ThreadFactory threadFactory
     final ThreadPoolExecutor exexutor = new ThreadPoolExecutor(10, 600, 30, TimeUnit.SECONDS, queue, Executors.defaultThreadFactory());
     final AtomicInteger completeTask = new AtomicInteger(0);
     final AtomicInteger rejectedTask = new AtomicInteger(0);
     static long beginTime;
     final int count = 1000;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         beginTime = System.currentTimeMillis();
         ThreadPoolExecutorDemo demo = new ThreadPoolExecutorDemo();
         demo.start();
+        System.out.println("被拒绝的任务数为:" + demo.rejectedTask.get());
+        System.out.println("执行完成的任务数为:" + demo.completeTask.get());
+        System.out.println("线程池峰值:" + demo.exexutor.getLargestPoolSize());
+        System.out.println("线程池总共处理的任务数" + demo.exexutor.getCompletedTaskCount());
     }
 
     private void start() {
         CountDownLatch latch = new CountDownLatch(count);
         CyclicBarrier barrier = new CyclicBarrier(count);
-        for(int i = 0; i< count; i++){
-            new Thread(new TestThread(latch, barrier)).start();
+        for (int i = 0; i < count; i++) {
+            Thread t = new Thread(new TestThread(latch, barrier));
+            t.setName("第" + i + "个线程");
+            t.start();
         }
-        try{
+        try {
             latch.await();
             exexutor.shutdownNow();
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
@@ -40,6 +53,7 @@ public class ThreadPoolExecutorDemo {
     private class TestThread implements Runnable {
         private CountDownLatch latch;
         private CyclicBarrier barrier;
+
         public TestThread(CountDownLatch latch, CyclicBarrier barrier) {
             this.latch = latch;
             this.barrier = barrier;
@@ -54,17 +68,19 @@ public class ThreadPoolExecutorDemo {
             } catch (BrokenBarrierException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
-            try{
+            try {
                 exexutor.execute(new Task(latch));
-            }catch (RejectedExecutionException e){
+            } catch (RejectedExecutionException e) {
                 latch.countDown();
-                System.out.println(rejectedTask.incrementAndGet());
+                rejectedTask.incrementAndGet();
+                System.out.println("被拒绝的任务:" + Thread.currentThread().getName());
             }
         }
     }
 
     private class Task implements Runnable {
         private CountDownLatch latch;
+
         public Task(CountDownLatch latch) {
             this.latch = latch;
         }
@@ -72,12 +88,12 @@ public class ThreadPoolExecutorDemo {
         @Override
         public void run() {
             try {
-                Thread.sleep(2000) ;
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
-            System.out.println(completeTask.incrementAndGet());
-            System.out.println(System.currentTimeMillis() - beginTime);
+            completeTask.incrementAndGet();
+            System.out.println("执行完成的任务数:" + Thread.currentThread().getName() + "任务耗时为:" + (System.currentTimeMillis() - beginTime));
             latch.countDown();
         }
     }
